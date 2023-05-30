fd837f9a4882b9f852b4935bfbccc8c024bac6bd
Lauren DeLago <123128869+lcarinad@users.noreply.github.com>
2023-05-30 12:55:49 -0400

    finished specs for payments test

--- payment.test.js
+++ payment.test.js
@@ -11,4 +11,46 @@ describe("payment func test", function () {
     expect(allPayments["payment1"].tipAmt).toEqual("40");
     expect(allPayments["payment1"].tipPercent).toEqual(20);
   });
+
+  it("should not add new payment to table if input is empty", function () {
+    billAmtInput.value = "";
+    submitPaymentInfo();
+    expect(Object.keys(allPayments).length).toEqual(0);
+  });
+  it("should payment update #paymentTable on appendPaymentTable func", function () {
+    let curPayment = createCurPayment();
+    allPayments["payment1"] = curPayment;
+    appendPaymentTable(curPayment);
+    let curTdList = document.querySelectorAll("#paymentTable tbody tr td");
+    expect(curTdList.length).toEqual(3);
+    expect(curTdList[0].innerText).toEqual("$200");
+    expect(curTdList[1].innerText).toEqual("$40");
+    expect(curTdList[2].innerText).toEqual("20%");
+  });
+  it("should create new payment on create curPayment func", function () {
+    let expectedPayment = {
+      billAmt: "200",
+      tipAmt: "40",
+      tipPercent: 20,
+    };
+
+    expect(createCurPayment()).toEqual(expectedPayment);
+  });
+  it("should not create payment with empty input on createCurrPayment func", function () {
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
+    let curPayment = createCurPayment();
+    expect(curPayment).toEqual(undefined);
+  });
+  afterEach(function () {
+    billAmtInput.value = "";
+    tipAmtInput.value = "";
+    paymentTbody.innerHTML = "";
+    summaryTds[0].innerHTML = "";
+    summaryTds[1].innerHTML = "";
+    summaryTds[2].innerHTML = "";
+    serverTbody.innerHTML = "";
+    paymentId = 0;
+    allPayments = {};
+  });
 });
